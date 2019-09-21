cd380f2fb608f63ba3806be437f2ae375bf10c91
grcanosa <grcanosa@gmail.com>
2019-09-21 19:41:38 +0200

    Added mosquitto.conf

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 
 git clone https://github.com/Koenkk/zigbee-shepherd-converters.git ./zigbee2mqtt/zigbee-shepherd-converters
 wget https://raw.githubusercontent.com/Koenkk/zigbee2mqtt/master/lib/extension/homeassistant.js -O ./zigbee2mqtt/lib/extension/homeassistant.js
+mkdir -p mosquitto/data
