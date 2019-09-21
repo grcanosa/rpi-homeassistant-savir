8f7479c50415ecdec7f6a644df41a05d740c7fbb
grcanosa <grcanosa@gmail.com>
2019-09-21 19:38:13 +0200

    Added setup

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/Koenkk/zigbee-shepherd-converters.git ./zigbee2mqtt/zigbee-shepherd-converters
+wget https://raw.githubusercontent.com/Koenkk/zigbee2mqtt/master/lib/extension/homeassistant.js -O ./zigbee2mqtt/lib/extension/homeassistant.js
